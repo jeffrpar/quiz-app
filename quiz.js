7a0d087af8983b520ae7e52ec6785b812e859f4e
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-05-10 16:17:00 -0500

    fixed typo in quiz.js function

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 const question = document.querySelector("#question");
-const choices = Array.from(querySelectorAll(".choice-text"));
+const choices = Array.from(document.querySelectorAll(".choice-text"));
 const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
@@ -57,10 +57,10 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign('/end.html')
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+      localStorage.setItem("mostRecentScore", score);
+  
+      return (window.location.href = "end.html");
     }
 
     questionCounter++
